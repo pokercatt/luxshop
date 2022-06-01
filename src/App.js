cc55a88775d0d85b7464d52aa8e0f3a27dc53fd6
Tim <timnar@icloud.com>
2022-06-01 16:47:10 +0200

    redux core set

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<ProductListing />}></Route>
+          <Route
+            path="/product/:productId"
+            exact
+            element={<ProductDetail />}
+          ></Route>
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
